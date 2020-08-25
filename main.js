bdc1963c31fc483091e9fd3a25d1a2a440399b70
d06hara <d06.harada.prg@gmail.com>
2020-08-25 21:02:16 +0900

    ページ内リンク実装

--- main.js
+++ main.js
@@ -2,6 +2,29 @@ $(".dropdown").dropdown();
 
 // ヘッダーの高さ分bodyを下げる
 $(function () {
-  let header_height = $("#header-menu").height();
+  var header_height = $("#header-menu").height();
   $("body").css("margin-top", header_height + 10);
+
+  $("a[href]").click(function () {
+    var speed = 1000;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    // console.log("aaa");
+    var position = target.offset().top - header_height - 20;
+    $("body,html").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
 });
+
+// // ページ内リンクのずれ解消
+// $(function () {
+//   // let windowWidth = $(window).width();
+//   $("a[href^=#]").click(function () {
+//     var speed = 1000;
+//     var href = $(this).attr("href");
+//     var target = $(href == "#" || href == "" ? "html" : href);
+//     var position = target.offset().top - header_height;
+//     $("body,html").animate({ scrollTop: position }, speed, "swing");
+//     return false;
+//   });
+// });
